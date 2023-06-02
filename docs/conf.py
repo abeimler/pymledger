803cc218a1b22917b4e9e3f95da0ca2dbf3d3f84
abeimler <alex-beimler@web.de>
2023-06-02 12:19:57 +0200

    feat: add github pages CI

--- docs/conf.py
+++ docs/conf.py
@@ -6,8 +6,12 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import sys
+from os.path import abspath, dirname
 from sphinx.highlighting import lexers
 from pygments.lexers.data import YamlLexer
+
+sys.path.insert(0, abspath(dirname(dirname(dirname(__file__)))))
 
 project = 'pYMLedger'
 copyright = '2023, Alex Beimler'
@@ -18,14 +22,17 @@ version = '0.1.0'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinxcontrib.spelling',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.githubpages'
+    #'sphinxcontrib.spelling',
 ]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 language = 'de'
-spelling_lang = 'de_DE'
+#spelling_lang = 'de_DE'
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
